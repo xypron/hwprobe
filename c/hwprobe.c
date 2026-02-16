173d6799b7fad2d54f686dc2404704180eaffc77
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2026-02-16 23:26:14 +0100

    Add support for RISCV_HWPROBE_KEY_IMA_EXT_1
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- c/hwprobe.c
+++ c/hwprobe.c
@@ -14,6 +14,7 @@
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
 struct ext_desc {
+	int probe_item;
 	long long key;
 	const char *text;
 	int required;
@@ -56,73 +57,75 @@ error:
 
 int main()
 {
+	size_t probe_item_count;
 	unsigned int version;
-	struct riscv_hwprobe probe_items[2];
+	struct riscv_hwprobe probe_items[3];
 	struct ext_desc exts[] = {
-		{RISCV_HWPROBE_IMA_FD, "F and D", 1, 0},
-		{RISCV_HWPROBE_IMA_C, "C", 1, 0},
-		{RISCV_HWPROBE_IMA_V, "V", 1, 0},
-		{RISCV_HWPROBE_EXT_ZBA, "Zba", 1, 0},
-		{RISCV_HWPROBE_EXT_ZBB, "Zbb", 1, 0},
-		{RISCV_HWPROBE_EXT_ZBS, "Zbs", 1, 0},
-		{RISCV_HWPROBE_EXT_ZICBOZ, "Zicboz", 1, 0},
-		{RISCV_HWPROBE_EXT_ZBC, "Zbc", 0, 0},
-		{RISCV_HWPROBE_EXT_ZBKB, "Zbkb", 0, 0},
-		{RISCV_HWPROBE_EXT_ZBKC, "Zbkc", 0, 0},
-		{RISCV_HWPROBE_EXT_ZBKX, "Zbkx", 0, 0},
-		{RISCV_HWPROBE_EXT_ZKND, "Zknd", 0, 0},
-		{RISCV_HWPROBE_EXT_ZKNE, "Zkne", 0, 0},
-		{RISCV_HWPROBE_EXT_ZKNH, "Zknh", 0, 0},
-		{RISCV_HWPROBE_EXT_ZKSED, "Zksed", 0, 0},
-		{RISCV_HWPROBE_EXT_ZKSH, "Zksh", 0, 0},
-		{RISCV_HWPROBE_EXT_ZKT, "Zkt", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVBB, "Zvbb", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVBC, "Zvbc", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKB, "Zvkb", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVKG, "Zvkg", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKNED, "Zvkned", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKNHA, "Zvknha", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKNHB, "Zvknhb", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKSED, "Zvksed", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKSH, "Zvksh", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVKT, "Zvkt", 1, 0},
-		{RISCV_HWPROBE_EXT_ZFH, "Zfh", 0, 0},
-		{RISCV_HWPROBE_EXT_ZFHMIN, "Zfhmin", 1, 0},
-		{RISCV_HWPROBE_EXT_ZIHINTNTL, "Zihintntl", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVFH, "Zvfh", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVFHMIN, "Zvfhmin", 1, 0},
-		{RISCV_HWPROBE_EXT_ZFA, "Zfa", 1, 0},
-		{RISCV_HWPROBE_EXT_ZTSO, "Ztso", 0, 0},
-		{RISCV_HWPROBE_EXT_ZACAS, "Zacas", 0, 0},
-		{RISCV_HWPROBE_EXT_ZICNTR, "Zicntr", 1, 0x0006000f}, // 6.15
-		{RISCV_HWPROBE_EXT_ZICOND, "Zicond", 1, 0},
-		{RISCV_HWPROBE_EXT_ZIHINTPAUSE, "Zihintpause", 1, 0},
-		{RISCV_HWPROBE_EXT_ZIHPM, "Zihpm", 1, 0x0006000f}, // 6.15
-		{RISCV_HWPROBE_EXT_ZVE32X, "Zve32x", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVE32F, "Zve32f", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVE64X, "Zve64x", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVE64F, "Zve64f", 1, 0},
-		{RISCV_HWPROBE_EXT_ZVE64D, "Zfe64d", 1, 0},
-		{RISCV_HWPROBE_EXT_ZIMOP, "Zimop", 1, 0},
-		{RISCV_HWPROBE_EXT_ZCA, "Zca", 1, 0},
-		{RISCV_HWPROBE_EXT_ZCB, "Zcb", 1, 0},
-		{RISCV_HWPROBE_EXT_ZCD, "Zcd", 1, 0},
-		{RISCV_HWPROBE_EXT_ZCF, "Zcf", 0, 0},
-		{RISCV_HWPROBE_EXT_ZCMOP, "Zcmop", 1, 0},
-		{RISCV_HWPROBE_EXT_ZAWRS, "Zawrs", 1, 0},
-		{RISCV_HWPROBE_EXT_ZAAMO, "Zaamo", 1, 0x0006000f}, // 6.15
-		{RISCV_HWPROBE_EXT_ZALRSC, "Zalrsc", 1, 0x0006000f}, // 6.15
-		{RISCV_HWPROBE_EXT_SUPM, "Supm", 1, 0x0006000d}, // 6.13
-		{RISCV_HWPROBE_EXT_ZFBFMIN, "Zfbfmin", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVFBFMIN, "Zvfbfmin", 0, 0},
-		{RISCV_HWPROBE_EXT_ZVFBFWMA, "Zvfbfwma", 0, 0},
-		{RISCV_HWPROBE_EXT_ZICBOM, "Zicbom", 1, 0x0006000f}, // 6.15
-		{RISCV_HWPROBE_EXT_ZABHA, "Zabha", 0, 0},
-		{RISCV_HWPROBE_EXT_ZALASR, "Zalasr", 0, 0},
-		{RISCV_HWPROBE_EXT_ZICBOP, "Zicbop", 1, 0x00060013}, // 6.19
-		{RISCV_HWPROBE_EXT_ZILSD, "Zilsd", 0, 0},
-		{RISCV_HWPROBE_EXT_ZCLSD, "Zclsd", 0, 0},
-		{RISCV_HWPROBE_EXT_ZICFILP, "Zicfilp", 0, 0},
+		{1, RISCV_HWPROBE_IMA_FD, "F and D", 1, 0},
+		{1, RISCV_HWPROBE_IMA_C, "C", 1, 0},
+		{1, RISCV_HWPROBE_IMA_V, "V", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZBA, "Zba", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZBB, "Zbb", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZBS, "Zbs", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZICBOZ, "Zicboz", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZBC, "Zbc", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZBKB, "Zbkb", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZBKC, "Zbkc", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZBKX, "Zbkx", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZKND, "Zknd", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZKNE, "Zkne", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZKNH, "Zknh", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZKSED, "Zksed", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZKSH, "Zksh", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZKT, "Zkt", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVBB, "Zvbb", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVBC, "Zvbc", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKB, "Zvkb", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKG, "Zvkg", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKNED, "Zvkned", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKNHA, "Zvknha", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKNHB, "Zvknhb", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKSED, "Zvksed", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKSH, "Zvksh", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVKT, "Zvkt", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZFH, "Zfh", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZFHMIN, "Zfhmin", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZIHINTNTL, "Zihintntl", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVFH, "Zvfh", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVFHMIN, "Zvfhmin", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZFA, "Zfa", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZTSO, "Ztso", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZACAS, "Zacas", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZICNTR, "Zicntr", 1, 0x0006000f}, // 6.15
+		{1, RISCV_HWPROBE_EXT_ZICOND, "Zicond", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZIHINTPAUSE, "Zihintpause", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZIHPM, "Zihpm", 1, 0x0006000f}, // 6.15
+		{1, RISCV_HWPROBE_EXT_ZVE32X, "Zve32x", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVE32F, "Zve32f", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVE64X, "Zve64x", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVE64F, "Zve64f", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZVE64D, "Zfe64d", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZIMOP, "Zimop", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZCA, "Zca", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZCB, "Zcb", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZCD, "Zcd", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZCF, "Zcf", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZCMOP, "Zcmop", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZAWRS, "Zawrs", 1, 0},
+		{1, RISCV_HWPROBE_EXT_ZAAMO, "Zaamo", 1, 0x0006000f}, // 6.15
+		{1, RISCV_HWPROBE_EXT_ZALRSC, "Zalrsc", 1, 0x0006000f}, // 6.15
+		{1, RISCV_HWPROBE_EXT_SUPM, "Supm", 1, 0x0006000d}, // 6.13
+		{1, RISCV_HWPROBE_EXT_ZFBFMIN, "Zfbfmin", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVFBFMIN, "Zvfbfmin", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZVFBFWMA, "Zvfbfwma", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZICBOM, "Zicbom", 1, 0x0006000f}, // 6.15
+		{1, RISCV_HWPROBE_EXT_ZABHA, "Zabha", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZALASR, "Zalasr", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZICBOP, "Zicbop", 1, 0x00060013}, // 6.19
+		{1, RISCV_HWPROBE_EXT_ZILSD, "Zilsd", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZCLSD, "Zclsd", 0, 0},
+		{1, RISCV_HWPROBE_EXT_ZICFILP, "Zicfilp", 0, 0},
+		{2, RISCV_HWPROBE_EXT_ZICFISS, "Zicfiss", 0, 0},
 	};
 	long ret;
 
@@ -130,11 +133,18 @@ int main()
 	probe_items[0].value = 0;
 	probe_items[1].key = RISCV_HWPROBE_KEY_IMA_EXT_0;
 	probe_items[1].value = 0;
+	probe_items[2].key = RISCV_HWPROBE_KEY_IMA_EXT_1;
+	probe_items[2].value = 0;
 
 	version = kernel_version();
+	probe_item_count = 2;
+	if (version >= 0x00070000) {
+		probe_item_count = 3;
+	}
 
 	// Call the hwprobe syscall
-	ret = syscall(SYS_riscv_hwprobe, probe_items, 2, 0, NULL, 0);
+	ret = syscall(SYS_riscv_hwprobe, probe_items,
+		      probe_item_count, 0, NULL, 0);
 	if (ret) {
 		perror("sys_riscv_hwprobe failed");
 
@@ -150,7 +160,10 @@ int main()
 
 	// Check extensions
 	for (size_t i = 0; i < ARRAY_SIZE(exts); ++i) {
-		if (!(probe_items[1].value & exts[i].key) &&
+		int probe_item = exts[i].probe_item;
+
+		if ((probe_item >= probe_item_count ||
+		     !(probe_items[probe_item].value & exts[i].key)) &&
 		    exts[i].required && exts[i].introduced <= version) {
 			printf("%s NOT supported\n", exts[i].text);
 			return EXIT_FAILURE;
